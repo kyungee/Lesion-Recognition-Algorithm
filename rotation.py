c9b7fba2ad098fcc213c47183cb58e59618e5d1c
김경이 <kyungee.kim@gmail.com>
2018-05-03 17:24:33 +0900

    rotation - 모든 디렉토리 순회하도록 코드 추가 dataset 폴더 구조 모두 순회하는 rotation 코드 추가

--- rotation.py
+++ rotation.py
@@ -3,28 +3,37 @@ import os, sys
 import cv2
 
 
-path_dir = './data/test/ruptured/base/'
-file_list = os.listdir(path_dir)
+all_data_dir = ['./data/test/ruptured/base/', './data/test/ruptured/mark/',
+                './data/test/unruptured/base/', './data/test/unruptured/mark/',
+                './data/train/ruptured/base/', './data/train/ruptured/mark/',
+                './data/train/unruptured/base/', './data/train/unruptured/mark/']
 
-file_list.remove('.DS_Store')
+for path_dir in all_data_dir:
+    #path_dir = './data/test/ruptured/base/'
+    file_list = os.listdir(path_dir)
 
-for filename in file_list:
-    input_filename = path_dir+filename
-    print(input_filename)
+    if '.DS_Store' in file_list:
+        file_list.remove('.DS_Store')
 
-    for i in range(1, 4):
-        img = cv2.imread(input_filename, 1)
-        rows, cols = img.shape[:2]
-        angle = i*90
+    for filename in file_list:
+        input_filename = path_dir+filename
+        print(input_filename)
 
-        M = cv2.getRotationMatrix2D((cols / 2, rows / 2), -angle, 1)
-        dst = cv2.warpAffine(img, M, (cols, rows))
+        for i in range(1, 4):
+            img = cv2.imread(input_filename, 1)
+            rows, cols = img.shape[:2]
+            angle = i*90
 
-        f_filename = filename.split('.jpg')[0]
+            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), -angle, 1)
+            dst = cv2.warpAffine(img, M, (cols, rows))
 
-        output_filename = f_filename+'_rotate_'+str(angle)+'.jpg'
-        cv2.imwrite('./result/'+output_filename, dst)
+            f_filename = filename.split('.jpg')[0]
 
+            output_filename = f_filename+'_rotate_'+str(angle)+'.jpg'
+            cv2.imwrite('./result/'+path_dir[7:]+output_filename, dst)
+
+
+print("\n * Completed!!!!");
 
 #cv2.imshow('Original', img)
 #cv2.imshow('warpAffine', dst)
