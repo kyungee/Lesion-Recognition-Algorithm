ab5aa4f271d26be19a9a34f74c18d647e8cbc85f
김경이 <kyungee.kim@gmail.com>
2018-05-03 21:42:22 +0900

    rotation 중복 코드 제거 및 수정

--- rotation.py
+++ rotation.py
@@ -8,8 +8,8 @@ all_data_dir = ['./data/test/ruptured/base/', './data/test/ruptured/mark/',
                 './data/train/ruptured/base/', './data/train/ruptured/mark/',
                 './data/train/unruptured/base/', './data/train/unruptured/mark/']
 
+
 for path_dir in all_data_dir:
-    #path_dir = './data/test/ruptured/base/'
     file_list = os.listdir(path_dir)
 
     if '.DS_Store' in file_list:
@@ -19,21 +19,22 @@ for path_dir in all_data_dir:
         input_filename = path_dir+filename
         print(input_filename)
 
-        for i in range(1, 4):
-            img = cv2.imread(input_filename, 1)
-            rows, cols = img.shape[:2]
-            angle = i*90
+        img = cv2.imread(input_filename, 1)
+        rows, cols = img.shape[:2]
 
+        for i in range(0, 4):
+            angle = i*90
             M = cv2.getRotationMatrix2D((cols / 2, rows / 2), -angle, 1)
             dst = cv2.warpAffine(img, M, (cols, rows))
 
             f_filename = filename.split('.jpg')[0]
-
             output_filename = f_filename+'_rotate_'+str(angle)+'.jpg'
-            cv2.imwrite('./result/'+path_dir[7:]+output_filename, dst)
+
+            path = r'./result/%s%s' % (path_dir[7:], output_filename)
+            cv2.imwrite(path, dst)
 
 
-print("\n * Completed!!!!");
+print("\n * Completed!!!!")
 
 #cv2.imshow('Original', img)
 #cv2.imshow('warpAffine', dst)
